142927ff6a13d89ad71acfc98cfef1e02ac96e02
winstonbarbe <winston.barbe@gmail.com>
2020-09-26 17:43:14 -0400

    Adds models for genres and genremovies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,11 @@ class Movie < ApplicationRecord
   validates :plot, length: { in: 10..500 }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.pluck(:name)
+  end
+
 end
